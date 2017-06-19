8b1e2b130396b4eac57a963900ba4053fecb75d8
Peter Shih <pihsun@chromium.org>
2017-06-19 17:08:48 +0800

    overlord: Fix lint errors in app/common/js/utils.js
    BUG=None
    TEST=make test
    
    Change-Id: I56883b2afef01fa3b95264e7ab4e8823ad2602e5
    Reviewed-on: https://chromium-review.googlesource.com/538386
    Commit-Ready: Pi-Hsun Shih <pihsun@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Reviewed-by: Youcheng Syu <youcheng@chromium.org>

--- go/src/overlord/app/common/js/utils.js
+++ go/src/overlord/app/common/js/utils.js
@@ -4,46 +4,46 @@
 
 function abbr(str, len) {
   if (str.length > len) {
-    return str.substr(0, len - 3) + "...";
+    return str.substr(0, len - 3) + '...';
   }
-  return str
+  return str;
 }
 
 function ReadBlobAsText(blob, callback) {
   var reader = new FileReader();
-  reader.addEventListener("loadend", function() {
+  reader.addEventListener('loadend', function() {
     callback(reader.result);
   });
   reader.readAsText(blob);
 }
 
 function randomID() {
-  return Math.random().toString(36).replace(/[^a-z]+/g, "").substr(0, 10);
+  return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 10);
 }
 
 // Execute Overlord remote shell command
 function getRemoteCmdOutput(mid, cmd) {
-  var url = "ws" + ((window.location.protocol == "https:")? "s": "" ) +
-            "://" + window.location.host + "/api/agent/shell/" + mid +
-            "?command=" + cmd;
+  var url = 'ws' + ((window.location.protocol == 'https:') ? 's' : '') +
+            '://' + window.location.host + '/api/agent/shell/' + mid +
+            '?command=' + cmd;
   var sock = new WebSocket(url);
   var deferred = $.Deferred();
 
-  sock.onopen = function (event) {
+  sock.onopen = function(event) {
     var blobs = [];
-    sock.onmessage = function (msg) {
+    sock.onmessage = function(msg) {
       if (msg.data instanceof Blob) {
         blobs.push(msg.data);
       }
-    }
-    sock.onclose = function (event) {
-      var value = "";
+    };
+    sock.onclose = function(event) {
+      var value = '';
       if (blobs.length == 0) {
-        deferred.resolve("");
+        deferred.resolve('');
       }
       for (var i = 0; i < blobs.length; i++) {
-        ReadBlobAsText(blobs[i], function (current) {
-          return function (text) {
+        ReadBlobAsText(blobs[i], function(current) {
+          return function(text) {
             value += text;
             if (current == blobs.length - 1) {
               deferred.resolve(value);
@@ -51,7 +51,7 @@ function getRemoteCmdOutput(mid, cmd) {
           }
         }(i));
       }
-    }
-  }
+    };
+  };
   return deferred.promise();
 }
