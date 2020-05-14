6b06182703619578ae57a3b03497f243e1556a20
Yilin Yang <kerker@google.com>
2020-05-14 13:52:30 +0800

    factory: Use `dict.items` instead of `six.iteritems`
    `six.iteritems` is equivalent to `dict.items` in python3, so we can
    migrate all of this.
    
    BUG=chromium:999876
    TEST=make test
    
    Change-Id: I7218c416e694da41996ae3ace6ce23e6f9fcfb72
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2200487
    Reviewed-by: Yilin Yang (kerker) <kerker@chromium.org>
    Tested-by: Yilin Yang (kerker) <kerker@chromium.org>
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>

--- py/tools/ovl.py
+++ py/tools/ovl.py
@@ -39,7 +39,6 @@ import urllib.request
 import jsonrpclib
 from jsonrpclib.config import Config
 from jsonrpclib.SimpleJSONRPCServer import SimpleJSONRPCServer
-from six import iteritems
 from ws4py.client import WebSocketBaseClient
 import yaml
 
@@ -672,7 +671,7 @@ def ParseMethodSubCommands(cls):
   This decorator retrieve command info from each method and append it in to the
   SUBCOMMANDS class variable, which is later used to construct parser.
   """
-  for unused_key, method in iteritems(cls.__dict__):
+  for unused_key, method in cls.__dict__.items():
     if hasattr(method, '__arg_attr'):
       # pylint: disable=protected-access
       cls.SUBCOMMANDS.append(method.__arg_attr)
