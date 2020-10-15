5f0e324234fd32053a29ff4913b80a62b5263ec1
Yilin Yang <kerker@google.com>
2020-10-15 14:52:17 +0800

    factory: Remove from __future__ import division
    Factory repo is python3 now, we can remove this compatible code.
    
    Use `ag 'from __future__ import division' -l | xargs sed -i '/^from
    __future__ import division$/N;/^from __future__ import division\n$/d'`
    to modify.
    
    BUG=None
    TEST=make test
    
    Change-Id: Icc4b70eac774cbf3cf9099e7179681f61f3b3185
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/platform/factory/+/2489281
    Tested-by: Yilin Yang (kerker) <kerker@chromium.org>
    Commit-Queue: Yilin Yang (kerker) <kerker@chromium.org>
    Reviewed-by: Jian-Jia Su <jjsu@chromium.org>

--- py/tools/ovl.py
+++ py/tools/ovl.py
@@ -3,8 +3,6 @@
 # Use of this source code is governed by a BSD-style license that can be
 # found in the LICENSE file.
 
-from __future__ import division
-
 import argparse
 import ast
 import base64
